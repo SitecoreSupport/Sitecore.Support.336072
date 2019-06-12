5c5e3c86a1d17a30dee2baa7d214b20a863bff46
DK\AMCH <amch@sitecore.net>
2019-06-12 14:57:39 +0300

    commit initial code

---
+++ src/Sitecore.Support.336072/Modules/EmailCampaign/Core/FieldValidators/EmailFieldValidator.cs
@@ -0,0 +1,52 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data.Validators;
+using Sitecore.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Validators;
+using System;
+using System.Runtime.Serialization;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.FieldValidators
+{
+    [Serializable]
+    public class EmailFieldValidator : StandardValidator
+    {
+        private RegexValidator _emailRegexValidator;
+
+        public EmailFieldValidator()
+        {
+        }
+
+        public EmailFieldValidator(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        protected override ValidatorResult Evaluate()
+        {
+            string controlValidationValue = base.ControlValidationValue;
+            if (string.IsNullOrEmpty(controlValidationValue) || this.EmailRegexValidator.IsValid(controlValidationValue.Trim()))
+            {
+                return ValidatorResult.Valid;
+            }
+            string[] arguments = new string[] { controlValidationValue.Trim() };
+            base.Text = this.GetText("'{0}' is not a valid email address.", arguments);
+            return base.GetFailedResult(ValidatorResult.CriticalError);
+        }
+
+        protected override ValidatorResult GetMaxValidatorResult() =>
+            base.GetFailedResult(ValidatorResult.CriticalError);
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            Assert.ArgumentNotNull(info, "info");
+            info.AddValue("Name", this.Name);
+            base.GetObjectData(info, context);
+        }
+
+
+        private RegexValidator EmailRegexValidator =>
+            (this._emailRegexValidator ?? (this._emailRegexValidator = Factory.CreateObject("emailRegexValidator", true) as RegexValidator));
+
+        public override string Name =>
+            "EmailFieldValidator";
+    }
+}
