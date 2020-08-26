5d4a7ff93241a0cf422aede69c076e1ee41b1afd
SC-AndreyMolchanov <amch@sitecore.net>
2020-08-26 17:34:22 +0300

    added fix for UrlFieldValidator

---
+++ src/Sitecore.Support.336072/Modules/EmailCampaign/Core/FieldValidators/UrlFieldValidator.cs
@@ -0,0 +1,53 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data.Validators;
+using Sitecore.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Validators;
+using System;
+using System.Runtime.Serialization;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.FieldValidators
+{
+    [Serializable]
+    public class UrlFieldValidator : StandardValidator
+    {
+        private RegexValidator _urlRegexValidator;
+
+        public override string Name => "UrlFieldValidator";
+
+        private RegexValidator UrlRegexValidator => _urlRegexValidator ?? (_urlRegexValidator = (Factory.CreateObject("urlRegexValidator", assert: true) as RegexValidator));
+
+        public UrlFieldValidator(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public UrlFieldValidator()
+        {
+        }
+
+        protected override ValidatorResult Evaluate()
+        {
+            string controlValidationValue = base.ControlValidationValue;
+            if (string.IsNullOrEmpty(controlValidationValue) || UrlRegexValidator.IsValid(controlValidationValue.Trim()))
+            {
+                return ValidatorResult.Valid;
+            }
+            base.Text = GetText("'{0}' is not a valid url.", controlValidationValue.Trim());
+            return GetFailedResult(ValidatorResult.CriticalError);
+        }
+
+        protected override ValidatorResult GetMaxValidatorResult()
+        {
+            return GetFailedResult(ValidatorResult.CriticalError);
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            Assert.ArgumentNotNull(info, "info");
+            #region commented the line since the same key-value is added in the base.GetObjectData method
+            //info.AddValue("Name", this.Name);
+            #endregion
+            base.GetObjectData(info, context);
+        }
+    }
+}
